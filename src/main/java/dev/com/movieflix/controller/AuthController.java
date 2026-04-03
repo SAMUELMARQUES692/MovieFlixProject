ddd1b8531d4b4a230ee1df62cd4b15b1aaed05da
samuel <samue@Samuel>
2026-04-03 13:33:52 -0300

    feat: Cria a Interface AuthController para documentar as classes Users no Swagger

--- src/main/java/dev/com/movieflix/controller/AuthController.java
+++ src/main/java/dev/com/movieflix/controller/AuthController.java
@@ -1,67 +1,47 @@
 package dev.com.movieflix.controller;
 
-import dev.com.movieflix.config.TokenService;
 import dev.com.movieflix.dto.UserDTO;
 import dev.com.movieflix.dto.request.LoginRequest;
 import dev.com.movieflix.dto.response.LoginResponse;
-import dev.com.movieflix.exception.UsernameOrPasswordInvalidException;
-import dev.com.movieflix.model.UserModel;
-import dev.com.movieflix.service.UserService;
-import lombok.RequiredArgsConstructor;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/movieflix/auth")
-@RequiredArgsConstructor
-public class AuthController {
+@Tag(name = "Users", description = "Recurso responsavel pelo gerenciamento dos Usuarios da aplicação")
+public interface AuthController {
 
-    private final UserService service;
-    private final AuthenticationManager authenticationManager;
-    private final TokenService tokenService;
-
+    @Operation(summary = "Salvar Usuarios", description = "Metodo responsavel por cadastrar e salvar novos usuarios no banco de dados")
+    @ApiResponse(responseCode = "201", description = "Usuario salvo com sucesso", content = @Content(schema = @Schema(implementation = UserDTO.class)))
     @PostMapping("/cadastrar")
-    public ResponseEntity<UserDTO> cadastrarUsuario(@RequestBody UserDTO userDTO) {
-        UserDTO novoUsuario = service.cadastrarUsuario(userDTO);
-        return ResponseEntity.ok(novoUsuario);
-    }
+    public ResponseEntity<UserDTO> cadastrarUsuario(@RequestBody UserDTO userDTO);
 
+    @Operation(summary = "Fazer Login", description = "Metodo responsavel por ver se o usuario existe no banco de dados e gerar um token para fazer as requisições das outras classes")
+    @ApiResponse(responseCode = "200", description = "Retorna o Token", content = @Content(schema = @Schema(implementation = LoginResponse.class)))
+    @ApiResponse(responseCode = "404", description = "Login ou senha invalidos", content = @Content())
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
-        try {
-            UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(request.email(), request.password());
-            Authentication authenticate = authenticationManager.authenticate(userAndPass);
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request);
 
-            UserModel userModel = (UserModel)authenticate.getPrincipal();
-
-            String token = tokenService.generateToken(userModel);
-
-            return ResponseEntity.ok(new LoginResponse(token));
-
-        }catch (BadCredentialsException exception) {
-            throw new UsernameOrPasswordInvalidException("Usuario ou senha invalido");
-        }
-    }
-
+    @Operation(summary = "Busca Usuarios", description = "Metodo responsavel por buscar todos os Usuarios cadastrados no banco de dados",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "200", description = "Retorna todos os usuarios cadastrados", content = @Content(array = @ArraySchema(schema = @Schema(implementation = UserDTO.class))))
     @GetMapping("/listar")
-    public ResponseEntity<List<UserDTO>> listarTodosUsuarios() {
-        List<UserDTO> users = service.listarUsuarios();
-        return ResponseEntity.ok(users);
-    }
+    public ResponseEntity<List<UserDTO>> listarTodosUsuarios();
 
+    @Operation(summary = "Deleta todos os Usuarios", description = "Metodo responsavel por deletar todos os usuarios cadastrados no banco de dados",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "204", description = "Usuarios deletados com sucesso", content = @Content())
     @DeleteMapping("/deletar")
-    public ResponseEntity<Void> deletarTodos() {
-        service.deleteAll();
-        return ResponseEntity.noContent().build();
-    }
-
-
-
+    public ResponseEntity<Void> deletarTodos();
 
 }
---
+++ src/main/java/dev/com/movieflix/controller/AuthControllerImpl.java
@@ -0,0 +1,67 @@
+package dev.com.movieflix.controller;
+
+import dev.com.movieflix.config.TokenService;
+import dev.com.movieflix.dto.UserDTO;
+import dev.com.movieflix.dto.request.LoginRequest;
+import dev.com.movieflix.dto.response.LoginResponse;
+import dev.com.movieflix.exception.UsernameOrPasswordInvalidException;
+import dev.com.movieflix.model.UserModel;
+import dev.com.movieflix.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movieflix/auth")
+@RequiredArgsConstructor
+public class AuthControllerImpl implements AuthController{
+
+    private final UserService service;
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<UserDTO> cadastrarUsuario(@RequestBody UserDTO userDTO) {
+        UserDTO novoUsuario = service.cadastrarUsuario(userDTO);
+        return ResponseEntity.ok(novoUsuario);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        try {
+            UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(request.email(), request.password());
+            Authentication authenticate = authenticationManager.authenticate(userAndPass);
+
+            UserModel userModel = (UserModel)authenticate.getPrincipal();
+
+            String token = tokenService.generateToken(userModel);
+
+            return ResponseEntity.ok(new LoginResponse(token));
+
+        }catch (BadCredentialsException exception) {
+            throw new UsernameOrPasswordInvalidException("Usuario ou senha invalido");
+        }
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<UserDTO>> listarTodosUsuarios() {
+        List<UserDTO> users = service.listarUsuarios();
+        return ResponseEntity.ok(users);
+    }
+
+    @DeleteMapping("/deletar")
+    public ResponseEntity<Void> deletarTodos() {
+        service.deleteAll();
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+
+}
